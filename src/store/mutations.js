6ac91caaec7287d067d65c7c7cbbd353549ec842
Antonio Natanael <antonionmc@tc.sumidenso.com.br>
2017-06-21 16:44:03 -0300

    Criando mutations para manipular as tasks

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,3 +1,17 @@
 export default {
   
+  NEW_TASK (state, task) {
+    state.tasks.push(task)
+  },
+
+  EDIT_TASK (state, task) {
+    let index = state.tasks.indexOf(task)
+    state.tasks[index] = task
+  },
+
+  DELETE_TASK (state, task) {
+    let index = state.tasks.indexOf(task)
+    state.tasks.splice(index, 1)
+  },
+
 }
