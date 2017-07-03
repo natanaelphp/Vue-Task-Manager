d2b23306e5871f5313ba5b9c2213c469593012c7
Antonio Natanael <antonionmc@tc.sumidenso.com.br>
2017-07-03 11:50:40 -0300

    Gravando as tasks no localStorage

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,17 +1,22 @@
+import { localStorageSetItem } from '../helpers/localStorage'
+
 export default {
-  
+
   NEW_TASK (state, task) {
     state.tasks.push(task)
+    localStorageSetItem('tasks', state.tasks)
   },
 
   EDIT_TASK (state, task) {
     let index = state.tasks.indexOf(task)
     state.tasks[index] = task
+    localStorageSetItem('tasks', state.tasks)
   },
 
   DELETE_TASK (state, task) {
     let index = state.tasks.indexOf(task)
     state.tasks.splice(index, 1)
+    localStorageSetItem('tasks', state.tasks)
   },
 
 }
